f4152315f4b78b4d565ddbb8a7cd7ed4744e6e9d
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-12-10 18:39:48 +0900

    Initial Commit

---
+++ Shifraise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shifraise
+//
+//  Created by 齊藤虎太郎 on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
